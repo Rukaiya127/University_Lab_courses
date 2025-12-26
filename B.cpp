833b294d33ef4c5e4ed0aac4d35e49efcb983507
Rukaiya Islam Ruku <141335570+Rukaiya127@users.noreply.github.com>
2025-12-26 16:48:34 +0600

    problem B

---
+++ B.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+
+        vector<string> a(n);
+        for (int i = 0; i < n; i++) {
+            cin >> a[i];
+        }
+
+        string s = "";
+        for (int i = 0; i < n; i++) {
+            string front = a[i] + s;
+            string back  = s + a[i];
+            s = min(front, back);
+        }
+
+        cout << s << '\n';
+    }
+
+    return 0;
+}
